27e778bc4a503c75727ebe6152024e793d13d1a1
Alina <al.litovckina@yandex.ru>
2022-09-24 17:32:56 +0300

    Добавлена работа с ссылками

--- src/main/kotlin/lab1/Main.kt
+++ src/main/kotlin/lab1/Main.kt
@@ -7,17 +7,20 @@ import java.net.URLEncoder
 
 fun main() {
     println("Введите запрос, информацию о котором хотите найти: ")
-    val key = readLine()
-    val question = "https://ru.wikipedia.org/w/api.php?action=query&list=search&utf8=&format=json&srsearch="+"$key"
+    var key = readLine()
+    key = URLEncoder.encode(key, "UTF-8")
+    var question = "https://ru.wikipedia.org/w/api.php?action=query&list=search&utf8=&format=json&srsearch=$key"
     println(question)
-    //question = URLEncoder.encode(question, "UTF-8")
 
-    val request ="{\"batchcomplete\":\"\",\"continue\":{\"sroffset\":10,\"continue\":\"-||\"},\"query\":{\"searchinfo\":{\"totalhits\":492},\"search\":[{\"ns\":0,\"title\":\"Санкт-Петербургский государственный электротехнический университет\",\"pageid\":11099,\"size\":56610,\"wordcount\":2101,\"snippet\":\"Санкт-Петербургский государственный электротехнический университет «<span class=\\\"searchmatch\\\">ЛЭТИ</span>» имени В. И. Ульянова (Ленина) — российское техническое высшее учебное заведение\",\"timestamp\":\"2022-09-15T17:22:38Z\"},{\"ns\":0,\"title\":\"Буревестник (баскетбольный клуб, Ленинград)\",\"pageid\":2235164,\"size\":5406,\"wordcount\":204,\"snippet\":\"мужская студенческая команда Ленинграда выступала в высшей лиге под названием <span class=\\\"searchmatch\\\">ЛЭТИ</span> (1951-52 гг.), «Наука» (1953-54 гг.) и, наконец, снова «Буревестник» (1955-61\",\"timestamp\":\"2022-04-17T23:30:41Z\"},{\"ns\":0,\"title\":\"Кафедра микро- и наноэлектроники Санкт-Петербургского государственного электротехнического университета\",\"pageid\":4619191,\"size\":33220,\"wordcount\":2143,\"snippet\":\"наноэлектроники СПбГЭТУ «<span class=\\\"searchmatch\\\">ЛЭТИ</span>» — кафедра факультета электроники Санкт-Петербургского государственного электротехнического университета «<span class=\\\"searchmatch\\\">ЛЭТИ</span>» им. В. И. Ульянова\",\"timestamp\":\"2022-09-19T13:57:19Z\"},{\"ns\":0,\"title\":\"Богородицкий, Николай Петрович\",\"pageid\":4619043,\"size\":19512,\"wordcount\":1066,\"snippet\":\"электротехнических материалов <span class=\\\"searchmatch\\\">ЛЭТИ</span> имени В. И. Ульянова (Ленина) (в настоящее время кафедра микро- и наноэлектроники). Ректор (1954—1967) <span class=\\\"searchmatch\\\">ЛЭТИ</span>. Один из основателей\",\"timestamp\":\"2022-08-08T18:25:34Z\"},{\"ns\":0,\"title\":\"Институт прикладной астрономии РАН\",\"pageid\":1122180,\"size\":12474,\"wordcount\":561,\"snippet\":\"Институт прикладной астрономии (полное официальное название — «Федеральное государственное бюджетное учреждение науки Институт прикладной астрономии Российской\",\"timestamp\":\"2022-04-03T20:02:14Z\"},{\"ns\":0,\"title\":\"Рубекин, Борис Александрович\",\"pageid\":518362,\"size\":4382,\"wordcount\":214,\"snippet\":\"города Ленинграда, закончил математический класс. В 1992-м году закончил <span class=\\\"searchmatch\\\">ЛЭТИ</span> (Ленинградский Электротехнический Институт имени Ульянова (Ленина)) по специальности\",\"timestamp\":\"2022-04-26T09:24:36Z\"},{\"ns\":0,\"title\":\"Мемориальный музей А. С. Попова (ЛЭТИ)\",\"pageid\":1664770,\"size\":8874,\"wordcount\":505,\"snippet\":\"Мемориальный музей А. С. Попова (<span class=\\\"searchmatch\\\">ЛЭТИ</span>) — музей А. С. Попова в Санкт-Петербургском государственном электротехническом университете «<span class=\\\"searchmatch\\\">ЛЭТИ</span>» имени В. И. Ульянова (Ленина)\",\"timestamp\":\"2022-09-20T08:01:12Z\"},{\"ns\":0,\"title\":\"Пузанков, Дмитрий Викторович\",\"pageid\":7449364,\"size\":8251,\"wordcount\":321,\"snippet\":\"степени (2001). Родился 3 июля 1944 года в Ленинграде. 1967 год — окончил <span class=\\\"searchmatch\\\">ЛЭТИ</span> по специальности «Математические и счётно-решающие приборы и устройства»\",\"timestamp\":\"2022-09-20T16:18:31Z\"},{\"ns\":0,\"title\":\"Пасынков, Владимир Васильевич\",\"pageid\":4622402,\"size\":14530,\"wordcount\":790,\"snippet\":\"наук (1955), профессор (1959). Кандидат технических наук (1943). Работал в <span class=\\\"searchmatch\\\">ЛЭТИ</span> в качестве ассистента (1940), доцента (1945), профессора (1952—1967 и 1984—2000)\",\"timestamp\":\"2021-04-17T22:38:10Z\"},{\"ns\":0,\"title\":\"Аптекарский остров\",\"pageid\":155291,\"size\":16303,\"wordcount\":867,\"snippet\":\"вуза: Санкт-Петербургский государственный электротехнический университет (<span class=\\\"searchmatch\\\">ЛЭТИ</span>) и Санкт-Петербургский государственный химико-фармацевтический университет\",\"timestamp\":\"2022-05-30T01:13:06Z\"}]}}"
+    val connection = URL(question).openConnection() as HttpURLConnection
+    val request = connection.inputStream.bufferedReader().readText()
     val listAnswers: List<Answer>? = parserUrlAnswer(request)
     println("Results of request: ${listAnswers}")
-
+//
     println("Введите номер страницы, которую хотите найти: ")
     val page = readLine()
-    val resultPage = "https://ru.wikipedia.org/w/api.php?action=query&list=search&utf8=&format=json&srsearch="+"$page"
+    val resultPage = "https://ru.wikipedia.org/w/index.php?curid="+"$page"
     println(resultPage)
+    val desk: Desktop = Desktop.getDesktop()
+    desk.browse(URI("https://ru.wikipedia.org/w/index.php?curid="+"$page"))
 }
