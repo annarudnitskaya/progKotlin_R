27b6a089e6938423b4479f219f48b53d86acae64
annarudnitskaya <75776048+annarudnitskaya@users.noreply.github.com>
2022-11-10 12:48:31 +0300

    Update WithFile.kt

--- src/main/kotlin/lab2/WithFile.kt
+++ src/main/kotlin/lab2/WithFile.kt
@@ -5,79 +5,90 @@ import lab1.Answer
 import java.io.BufferedReader
 import java.io.File
 import java.io.FileReader
-//import org.apache.commons.csv.CSVFormat
-//import org.apache.commons.csv.CSVParser
 import org.w3c.dom.Document
 import javax.xml.parsers.DocumentBuilderFactory
+
 
 class WithFile {
     // XML
     fun readXML(filePath: String):List<Address> {
-        val file = File(filePath).readLines()
+        val file = File(filePath).readText()
         val listAddresses = mutableListOf<Address>()
         val startTime = System.currentTimeMillis()
         val hashMap: HashMap<Address, Int> = HashMap()
-
+      //  val file1 = File("addressBook.txt")
 //        val bufferedReader = BufferedReader(FileReader(filePath))
 //        if (file.isEmpty()) return null
+        val splitString = file.split('\n')
         var perem: String
-        for (i in file.withIndex()) {
-            perem = i.toString()
-            //println(perem)
+//        for (i in file.withIndex()) {
+        for (str in splitString) {
+            perem = str
+            perem = perem.substringAfter("<?xml version=\"1.0\" encoding=\"utf-8\"?>").trim()
+            perem = perem.substringAfter("\n").trim()
+            perem = perem.substringAfter("<root>").trim()
+            perem = perem.substringAfter("\n").trim()
+            perem = perem.substringAfter("</root>").trim()
+            perem = perem.substringAfter("\n").trim()
 //            perem=perem.substringAfter("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" + "<root>").trim()
+//                perem = perem.substringAfter("<root>\n").trim()
+                perem = perem.substringAfter("<item city=\"").trim()
+                val city = perem.substringBefore("\"").trim() //city
+                //   println(city)
+                perem = perem.substringAfter("\" street=\"")
+                val street = perem.substringBefore("\"").trim() //street
 
-            perem = perem.substringAfter("<item city=\"").trim()
-            val city = perem.substringBefore("\"").trim() //city
-            println(city)
-            perem = perem.substringAfter("\" street=\"")
-            val street = perem.substringBefore("\"").trim() //street
+                perem = perem.substringAfter("\" house=\"").trim()
+                val house = perem.substringBefore("\"").trim() //house
 
-            perem = perem.substringAfter("\" house=\"").trim()
-            val house = perem.substringBefore("\"").trim() //house
+                perem = perem.substringAfter("\" floor=\"").trim()
+                val floorsNum = perem.substringBefore("\"").trim() //floors
 
-            perem = perem.substringAfter("\" floor=\"").trim()
-            val floorsNum = perem.substringBefore("\"").trim() //floors
 
-            perem = perem.substringAfter("\" />\n").trim()
+                println(city + " " + street + " " + house + " " +  floorsNum)
+                val fullInfo = Address(city, street, house, floorsNum)
+                File("addressBook.txt").appendText( city + " " + street + " " + house + " " +  floorsNum + "\n")
+                listAddresses.add(fullInfo)
+            }
 
-            val fullInfo = Address(city, street, house, floorsNum)
-
-            listAddresses.add(fullInfo)
-        }
         return listAddresses
-
-
     }
 
     // CSV
     fun readCSV(filePath: String):List<Address> {
-        val file = File(filePath).readLines()
+ //       val file = File(filePath).readLines()
+        val file = File(filePath).readText()
+
         val listAddresses = mutableListOf<Address>()
         val startTime = System.currentTimeMillis()
         val hashMap: HashMap<Address, Int> = HashMap()
-
+      //  val file1 = File("addressBook.txt")
 //        val bufferedReader = BufferedReader(FileReader("filePath"))
 //        if (file.isEmpty()) return null
+        val splitString = file.split('\n')
         var perem: String
-        for ((cout, i) in file.withIndex()) {
-            perem = i
-            perem=perem.substringAfter("\n").trim()
-            perem = perem.substringAfter("\"").trim()
-            val city = perem.substringBefore("\"").trim() //city
+        for (str in splitString) {
+                perem = str
+                perem = perem.substringAfter("\"city\";\"street\";\"house\";\"floor\"").trim()
 
-            perem = perem.substringAfter("\";\"")
-            val street = perem.substringBefore("\"").trim() //street
+                perem = perem.substringAfter("\n").trim()
 
-            perem = perem.substringAfter("\";").trim()
-            val house = perem.substringBefore(";").trim() //house
+                perem = perem.substringAfter("\"").trim()
+                val city = perem.substringBefore("\"").trim() //city
 
-            perem = perem.substringAfter(";").trim()
-            val floorsNum = perem.substringBefore(";").trim() //floors
+                perem = perem.substringAfter("\";\"")
+                val street = perem.substringBefore("\"").trim() //street
 
+                perem = perem.substringAfter("\";").trim()
+                val house = perem.substringBefore(";").trim() //house
 
-            val fullInfo = Address(city, street, house, floorsNum)
-            listAddresses.add(fullInfo)
-        }
+                perem = perem.substringAfter(";").trim()
+                val floorsNum = perem.substringBefore(";").trim() //floors
+                println(city + " " + street + " " + house + " " +  floorsNum)
+                val fullInfo = Address(city, street, house, floorsNum)
+                listAddresses.add(fullInfo)
+                File("addressBook.txt").appendText( city + " " + street + " " + house + " " +  floorsNum + "\n")
+            }
         return listAddresses
 
 
